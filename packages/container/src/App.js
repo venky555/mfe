a5d26f0184c92f477fe307921825e0ca6ab711d1
venky555 <venky5a6@gmail.com>
2020-12-22 19:13:13 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {mount} from 'marketing/MarketingApp';
+import MarketingApp from './components/MarketingApp';
+
+console.log(mount);
+
+export default () =>{
+    return <div> 
+    <h1>Hi there!</h1>;
+    <hr/>
+    <MarketingApp/>
+    </div>
+}
