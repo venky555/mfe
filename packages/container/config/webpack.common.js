481a7bfab665b383b17529d680d32b6957ec9bcf
venky555 <venky5a6@gmail.com>
2020-12-22 23:06:38 +0530

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
     module: {
         rules: [
@@ -13,6 +16,11 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ]
 
 };
